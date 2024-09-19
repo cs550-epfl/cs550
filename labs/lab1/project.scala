eca37e49f81f83d325889123d98be425952e7547
Viktor Kuncak <viktor.kuncak@epfl.ch>
2024-09-19 17:44:03 +0200

    New version of jars

--- labs/lab1/project.scala
+++ labs/lab1/project.scala
@@ -1,4 +1,5 @@
 //> using scala "3.5.0"
-//> using jar "stainless-library_2.13-0.9.8.1.jar"
+//> using jar stainless-library.jar  
+//> using jar stainless-library-sources.jar
 //> using dep "org.scalameta::munit::1.0.2"
 //> using options "-Wconf:msg=not.*?exhaustive:s,msg=pattern.*?specialized:s"
